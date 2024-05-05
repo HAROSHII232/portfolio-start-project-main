0128d6e30fe38b1ac42f142efad39d3bbdc70c4c
Ihar Voitau <igorgrime@gmail.com>
2024-05-06 00:14:57 +0300

    feat: add styles for footer

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -8,23 +8,45 @@ type FooterMenuPropsType = {
 export const FooterMenu = (props: FooterMenuPropsType) => {
   return (
     <StyledFooterMenu>
-      <span>{props.menuTitle}</span>
-      <ul>
+      <MenuTitle>{props.menuTitle}</MenuTitle>
+      <MenuList>
         {props.menuItems.map((item, index) => {
           return (
-            <li key={index}>
-              <a href="#">{item}</a>
-            </li>
+            <MenuItem key={index}>
+              <MenuLink href="#">{item}</MenuLink>
+            </MenuItem>
           );
         })}
-      </ul>
+      </MenuList>
     </StyledFooterMenu>
   );
 };
 
 const StyledFooterMenu = styled.nav`
   ul {
-    display: flex;
-    flex-direction: column;
   }
+`;
+
+const MenuTitle = styled.h3`
+  font-weight: 500;
+  font-size: 16px;
+  line-height: 160%;
+  letter-spacing: 0.06em;
+  margin-bottom: 12px;
+`;
+
+const MenuList = styled.ul`
+  display: flex;
+  flex-direction: column;
+`;
+
+const MenuItem = styled.li`
+  margin-bottom: 4px;
+`;
+
+const MenuLink = styled.a`
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 160%;
+  opacity: 0.6;
 `;
--- src/layout/footer/Footer.tsx
+++ src/layout/footer/Footer.tsx
@@ -15,13 +15,15 @@ export const Footer = () => {
       <Container>
         <FlexWrapper justify="space-between">
           <FooterLogo src={footerLogo} />
-          <FooterMenu menuItems={footerMenuItems} menuTitle="мЕНЮ" />
-          <FooterMenu menuItems={footerCatalogItems} menuTitle="каталог" />
+          <FooterMenu menuItems={footerMenuItems} menuTitle="МЕНЮ" />
+          <FooterMenu menuItems={footerCatalogItems} menuTitle="КАТАЛОГ" />
           <Contact>
             <ContactTitle>кОНТАКТЫ ДЛЯ СВЯЗИ</ContactTitle>
-            <Phone href="tel:(405) 555-0128">Телефон:(405) 555-0128</Phone>
+            <Phone href="tel:(405) 555-0128">
+              Телефон: <span>(405) 555-0128</span>
+            </Phone>
             <Email href="mailto:hello@createx.com">
-              Почта:hello@createx.com
+              Почта: <span>hello@createx.com</span>
             </Email>
             <SocialList>
               <SocialItem>
@@ -85,6 +87,24 @@ export const Footer = () => {
 
 const StyledFooter = styled.footer`
   background-color: ${theme.colors.secondaryBg};
+
+  ${Container} {
+    padding-top: 40px;
+    padding-bottom: 25px;
+
+    position: relative;
+
+    &::before {
+      content: "";
+      position: absolute;
+      left: 0;
+      top: 225px;
+      width: 100%;
+      height: 1px;
+      background-color: #fff;
+      opacity: 0.15;
+    }
+  }
 `;
 
 const FooterLogo = styled.img`
@@ -97,18 +117,58 @@ const Contact = styled.div`
   flex-direction: column;
 `;
 
-const ContactTitle = styled.div``;
+const ContactTitle = styled.h3`
+  font-weight: 500;
+  font-size: 16px;
+  line-height: 160%;
+  letter-spacing: 0.06em;
+  text-transform: uppercase;
+  margin-bottom: 12px;
+`;
 
-const Phone = styled.a``;
+const Phone = styled.a`
+  margin-bottom: 5px;
+  span {
+    opacity: 0.6;
+  }
+`;
 
-const Email = styled.a``;
+const Email = styled.a`
+  span {
+    opacity: 0.6;
+  }
+`;
 
 const SocialList = styled.ul`
   display: flex;
+  margin-top: 24px;
+  gap: 12px;
 `;
 
-const SocialItem = styled.li``;
+const SocialItem = styled.li`
+  width: 32px;
+  height: 32px;
+  background: #141414;
+  border-radius: 4px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+`;
 
-const SocialLink = styled.a``;
+const SocialLink = styled.a`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
 
-const Adress = styled.a``;
+const Adress = styled.a`
+  display: flex;
+  justify-content: center;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 175%;
+  text-align: center;
+  color: #fff;
+  margin-top: 50px;
+`;
